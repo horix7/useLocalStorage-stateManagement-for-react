c3d6b17401a26d36fbb62b120f4960f45d00a2ef
horix7 <horix77777@gmail.com>
2021-04-23 18:27:15 +0200

    removing listeners

--- example/src/components/testBox.tsx
+++ example/src/components/testBox.tsx
@@ -1,16 +1,8 @@
-import React, {   useState } from 'react'
+import React from 'react'
 import { Observer  } from "local-state";
 
 export default function TestBox  ( ) {
 
-        const [state, setState] = useState("")
-        
-    window.addEventListener('storage', () => {
-                console.log(localStorage.state)
-                setState("something")
-                console.log(state)
-        })
-        
 
         return (
             <Observer>
@@ -18,12 +10,8 @@ export default function TestBox  ( ) {
                     (functions: any, state: any) => {
                         return (
                             <div onClick={() => functions.setGlobals("none", "none")}>
-                                --------------------------------
                                 <br/>
                                 {state.input1}
-                                  <p>{JSON.stringify(state)}</p>
-
-                                now who us 
                             </div>
                         )
                     }
---
+++ src/functions/storageListener.ts
@@ -0,0 +1,9 @@
+
+
+export function logStorageChange(changes: any, area: any) {
+    const log = "Change in storage area: " + area + changes + "\n\n\n"
+
+    return log
+    
+  }
+  
